28a498efa44566b88d971eb659dfd7dce0a01483
Taranjit Singh <tsingh777@gmail.com>
2020-02-04 18:41:38 -0800

    Demo for swagger docs

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cacheControl = require('express-cache-controller');
 const { createPool } = require('mariadb');
 const config = require('./api/config');
 const routes = require('./api/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const port = process.env.NODE_ENV === 'development' ? '3001' : '3000';
@@ -16,6 +18,7 @@ app.use(cacheControl({ maxAge: 21600 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/v2', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.cacheControl = { noCache: true };
